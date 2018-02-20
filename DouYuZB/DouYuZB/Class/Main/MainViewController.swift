2d23d56b2359eba446218d0780818c4b8cba8676
张城 <zhangcheng@zhangchengdemacbook-pro.local>
2018-02-20 15:04:57 +0800

    项目分类

---
+++ DouYuZB/DouYuZB/Class/Main/MainViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MainViewController.swift
+//  DouYuZB
+//
+//  Created by 张城 on 2018/2/20.
+//  Copyright © 2018年 张城. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+
+}
